d8ef73e5a052812e5d2c960941ac767076df811b
eugenesation <eugenesmirnov03@gmail.com>
2021-09-22 00:09:18 +0300

    fixed for .travis.yml.

--- src/test/java/ua/com/nix/timeline/controller/PostControllerTest.java
+++ src/test/java/ua/com/nix/timeline/controller/PostControllerTest.java
@@ -1,15 +1,8 @@
 package ua.com.nix.timeline.controller;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ua.com.nix.timeline.model.Post;
 import ua.com.nix.timeline.repository.PostRepository;
@@ -17,10 +10,8 @@ import ua.com.nix.timeline.service.impl.PostServiceImpl;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class PostControllerTest {
@@ -59,7 +50,7 @@ class PostControllerTest {
     }
 
     @Test
-    void delete() throws Exception {
+    void delete() {
         Post post = new Post(1L, "text");
         postRepository.save(post);
 
--- src/test/java/ua/com/nix/timeline/service/impl/PostServiceImplTest.java
+++ src/test/java/ua/com/nix/timeline/service/impl/PostServiceImplTest.java
@@ -3,8 +3,6 @@ package ua.com.nix.timeline.service.impl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class PostServiceImplTest {
 
     @BeforeEach
